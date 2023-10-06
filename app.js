5c6a1aa50c74e7f64644a4c95ae0af147300314e
nabil Slimani <slimaninabil93350@gmail.com>
2023-10-06 23:24:55 +0200

    feat (app.js): add books and cors

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
 const express = require("express");
+// const cors = require("cors");
 
 const app = express();
 
+// app.use(cors());
+
 app.use((req, res, next) => {
-  console.log("Requête reçue !");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
+app.get("/api/books", (req, res) => {
+  const books = [
+    {
+      userId: "nabs",
+      title: "Le Dernier Jour d'un condamné",
+      author: "Victor Hugo",
+      imageUrl: "",
+      year: 1829,
+      genre: "Roman",
+      ratings: [
+        {
+          userId: "identifiant mongoDB",
+          grade: "5",
+        },
+      ],
+      averageRating: "",
+    },
+    {
+      userId: "nabs",
+      title: "Odes et Ballades",
+      author: "Victor Hugo",
+      imageUrl: "",
+      year: 1828,
+      genre: "Poésie",
+      ratings: [
+        {
+          userId: "identifiant mongoDB",
+          grade: "4",
+        },
+      ],
+      averageRating: "",
+    },
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
