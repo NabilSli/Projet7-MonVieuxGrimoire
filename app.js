3051419f617e0bc818f419c1e76781169bda1061
nabil Slimani <slimaninabil93350@gmail.com>
2023-10-08 14:54:05 +0200

    feat (app.js): add put and delete routes to the api

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 // const cors = require("cors");
 const mongoose = require("mongoose");
-
-const app = express();
+const book = require("./models/Book");
 
 mongoose
   .connect(
@@ -12,8 +11,9 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use(express.json());
+const app = express();
 
+app.use(express.json());
 // app.use(cors());
 
 app.use((req, res, next) => {
@@ -30,47 +30,71 @@ app.use((req, res, next) => {
 });
 
 app.post("api/books", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({ message: "Objet créé !" });
-  next();
+  const book = new Book({ ...req.body });
+  book
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistrée !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
-app.get("/api/books", (req, res) => {
-  const books = [
-    {
-      userId: "nabs",
-      title: "Le Dernier Jour d'un condamné",
-      author: "Victor Hugo",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/HugoLastDayCondemnedMan.jpg/250px-HugoLastDayCondemnedMan.jpg",
-      year: 1829,
-      genre: "Roman",
-      ratings: [
-        {
-          userId: "identifiant mongoDB",
-          grade: "5",
-        },
-      ],
-      averageRating: "5",
-    },
-    {
-      userId: "nabs",
-      title: "Odes et Ballades",
-      author: "Victor Hugo",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/HugoOdesBallades.jpg/250px-HugoOdesBallades.jpg",
-      year: 1828,
-      genre: "Poésie",
-      ratings: [
-        {
-          userId: "identifiant mongoDB",
-          grade: "4",
-        },
-      ],
-      averageRating: "2",
-    },
-  ];
-  res.status(200).json(books);
+app.put("api/books", (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("api/books", (req, res, next) => {
+  book
+    .deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet supprimé" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/books/:id", (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/books", (req, res, next) => {
+  book
+    .find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+  // const books = [
+  // {
+  //   userId: "nabs",
+  //   title: "Le Dernier Jour d'un condamné",
+  //   author: "Victor Hugo",
+  //   imageUrl:
+  //     "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/HugoLastDayCondemnedMan.jpg/250px-HugoLastDayCondemnedMan.jpg",
+  //   year: 1829,
+  //   genre: "Roman",
+  //   ratings: [
+  //     {
+  //       userId: "identifiant mongoDB",
+  //       grade: "5",
+  //     },
+  //   ],
+  //   averageRating: "5",
+  // },
+  // {
+  //   userId: "nabs",
+  //   title: "Odes et Ballades",
+  //   author: "Victor Hugo",
+  //   imageUrl:
+  //     "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/HugoOdesBallades.jpg/250px-HugoOdesBallades.jpg",
+  //   year: 1828,
+  //   genre: "Poésie",
+  //   ratings: [
+  //     {
+  //       userId: "identifiant mongoDB",
+  //       grade: "4",
+  //     },
+  //   ],
+  //   averageRating: "2",
+  // },
+  // ];
 });
 
 module.exports = app;
