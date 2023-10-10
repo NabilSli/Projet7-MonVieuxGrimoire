8b23054652a04ced843c5412d231d63b7a19ccc9
nabil Slimani <slimaninabil93350@gmail.com>
2023-10-10 12:15:14 +0200

    feat (auth.js): add authorization midelware to the routes

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middleware/auth");
+
 const bookCtrl = require("../controllers/books");
 
-router.post("/", bookCtrl.createBook);
-router.put("/:id", bookCtrl.modifyBook);
-router.delete("/:id", bookCtrl.deleteBook);
+router.post("/", auth, bookCtrl.createBook);
+router.put("/:id", auth, bookCtrl.modifyBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
 router.get("/:id", bookCtrl.getOneBook);
 router.get("/", bookCtrl.getAllBooks);
 
