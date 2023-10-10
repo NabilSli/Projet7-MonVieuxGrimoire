8a518005b6cdc0404eb1e01adcfc696f5b84bf56
nabil Slimani <slimaninabil93350@gmail.com>
2023-10-10 16:00:50 +0200

    chore (books.js): add auth to all routes

--- routes/books.js
+++ routes/books.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/", auth, multer, bookCtrl.createBook);
 router.put("/:id", auth, multer, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
-router.get("/:id", bookCtrl.getOneBook);
-router.get("/", bookCtrl.getAllBooks);
+router.get("/:id", auth, bookCtrl.getOneBook);
+router.get("/", auth, bookCtrl.getAllBooks);
 
 module.exports = router;
