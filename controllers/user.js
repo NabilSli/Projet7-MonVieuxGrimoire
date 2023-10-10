b4d70c8934dc3985bd804132a5d6cdc6bbd57eb7
nabil Slimani <slimaninabil93350@gmail.com>
2023-10-10 11:33:39 +0200

    feat (user.js): add login function

--- controllers/user.js
+++ controllers/user.js
@@ -12,10 +12,33 @@ exports.signup = (req, res, next) => {
       });
       user
         .save()
-        .then(() => res.status(200).json({ message: "Utilisateur crée !" }))
+        .then(() => res.status(201).json({ message: "Utilisateur crée !" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ enail: req.body.email })
+    .then((user) => {
+      if (user === null) {
+        return res
+          .status(401)
+          .json({ message: "identifiant ou mot de passe incorrecte" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res
+                .status(401)
+                .json({ message: "identifiant ou mot de passe incorrecte" });
+            } else {
+              res.status(200).json({ userId: user._id, token: "TOKEN" });
+            }
+          })
+          .catch((error) => res.status(500).json({ error }));
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
