dbab5573209ef8557fb9d811d311b1f09701a3d1
nabil Slimani <slimaninabil93350@gmail.com>
2023-11-27 12:01:44 +0100

    feat (multer-config.js): add filter to limit images to 1mo

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -16,5 +16,22 @@ const storage = multer.diskStorage({
     callback(null, name + Date.now() + "." + extension);
   },
 });
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 }, // Limite de 1 Mo (1 Mo = 1024 ko)
+  fileFilter: (req, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return callback(
+        new Error(
+          "Seuls les fichiers JPG, JPEG ou PNG de moins de 1mo sont autorisés."
+        )
+      );
+    }
+    callback(null, true);
+  },
+}).single("image");
+
+module.exports = upload;
 //TODO: tester en plus taille max de l'image (1mb), ajouter sharp pour ecrasement d'image
-module.exports = multer({ storage: storage }).single("image");
+// module.exports = multer({ storage: storage }).single("image");
