1498aa9ff24dff0acb87fdc66d793facdcd0ccf3
nabil Slimani <slimaninabil93350@gmail.com>
2023-11-27 18:33:13 +0100

    feat(.env.local): create the environment var and use them

---
+++ config.js
@@ -0,0 +1,6 @@
+const dotenv = require("dotenv");
+
+module.exports = {
+  endpoint: process.env.API_URL,
+  masterKey: process.env.API_KEY,
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
+const { masterKey } = require("../config");
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedtoken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedtoken = jwt.verify(token, masterKey);
     const userId = decodedtoken.userId;
     req.auth = {
       userId: userId,
